cb1399b49df07c1eb46129ef68307cf82ccc9b20
Joao Moura <jg.moura.almeida@gmail.com>
2019-09-26 16:37:18 -0500

    Adds main

---
+++ grammar/main.py
@@ -0,0 +1,20 @@
+import sys
+from antlr4 import *
+from otterLexer import otterLexer
+from otterParser import otterParser
+from antlr4.tree.Trees import Trees
+
+def main(argv):
+  input_stream = FileStream(argv[1])
+  lexer = otterLexer(input_stream)
+  stream = CommonTokenStream(lexer)
+  parser = otterParser(stream)
+  tree = parser.program()
+  print(Trees.toStringTree(tree, None, parser))
+
+  if parser.getNumberOfSyntaxErrors() == 0:
+    print("PROGRAMA CORRECTO")
+
+
+if __name__ == '__main__':
+  main(sys.argv)
