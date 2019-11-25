13433e08b2800878fde7e9ba615fff91da29b1b4
Rene Garcia <rene.saaenz@gmail.com>
2019-11-24 22:02:41 -0600

    Adds comments to stack

--- helpers/custom_stack.py
+++ helpers/custom_stack.py
@@ -1,24 +1,58 @@
+from typing import Any, List
+
 class Stack:
+    """ Simple implementatin of a stack.
+    """
     def __init__(self):
         self.__elements = []
 
-    def push(self, item):
+    def push(self, item: Any):
+        """ Pushes an item to the stack.
+
+            Arguments:
+                - item [Any]: The element to be pushed to the stack.
+        """
         self.__elements.append(item)
 
-    def pop(self):
+    def pop(self) -> Any:
+        """ Returns the top element in the stack and removes it from the stack.
+
+            Returns:
+                - [Any]: The top element of the stack.
+        """
         return self.__elements.pop()
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
+        """ Wether the stack is empty or not.
+
+            Returns:
+                - [bool]: Indicates if the stack is empty.
+        """
         return len(self.__elements) == 0
 
-    def top(self):
+    def top(self) -> Any:
+        """ Returns the top element in the stack or None if the stack is empty.
+
+            Returns:
+                - [Any]: The top element of the stack.
+        """
         if self.isEmpty():
             return None
         return self.__elements[len(self.__elements) - 1]
 
-    def size(self):
+    def size(self) -> int:
+        """ Returns the size of the stack.
+
+            Returns:
+                - [int]: The size of the stack.
+        """
         return len(self.__elements)
 
     @property
-    def elements(self):
+    def elements(self) -> List[Any]:
+        """ Returns all the elements in the stack as a list.
+
+            Returns:
+                - [List[Any]]: Elements in the stack a list.
+        """
         return self.__elements
