e89cee4e1ca86b89fa5088a4e1e45e6a485c6aad
Rene Garcia <rene.saaenz@gmail.com>
2019-11-24 20:59:02 -0600

    Adds comments to compiler

--- compilation/compiler.py
+++ compilation/compiler.py
@@ -188,38 +188,68 @@ class Compiler:
 
     @staticmethod
     def add_dimension(name: str, size: int):
+        """ Adds the dimension to a multi dimensional variable.
+
+            Arguments:
+                - name [str]: The name of the variable
+                - size [str]: The size of the dimension.
+        """
         variable = Compiler._current_method.variables_directory.search(name)
         variable.add_new_dimension(int(size))
 
     @staticmethod
     def populate_dimension_attributes(name: str):
+        """ Handler to compute the dimension of a variable.
+
+            Arguments:
+                - name [str]: The name of the variable.
+        """
         variable = Compiler._current_method.variables_directory.search(name)
         variable.populate_dimension_attributes()
         Compiler._current_method.allocate_memory_chunk(variable)
 
     @staticmethod
     def gen_quad_assign():
+        """ Compiler handler for assign quad.
+        """
         Compiler._interpreter.assign()
 
     @staticmethod
     def push_op(op):
+        """ Compiler handler for pushing an operator.
+        """
         Compiler._interpreter.push_operator(op)
 
     @staticmethod
     def open_par():
+        """ Compiler handler for open_par.
+        """
         Compiler._interpreter.open_par()
 
     @staticmethod
     def close_par():
+        """ Compiler handler for close_par.
+        """
         Compiler._interpreter.close_par()
 
     @staticmethod
-    def push_constant(type_, value):
+    def push_constant(type_: str, value: int):
+        """ Compiler handler for pushing a constant.
+
+            Arguments:
+                - type_ [str]: Type of constant.
+                - value [str]: Value of constant.
+        """
         memory_space = CompilationMemory.next_const_memory_space(value, type_)
         Compiler._interpreter.push_constant(type_, memory_space)
 
     @staticmethod
     def push_variable(name):
+        """ Compiler handler for pushing a variable.
+
+            Arguments:
+                - name [str]: Name of variable.
+        """
         current_scope = Compiler._current_method
         # logger.debug(current_scope.variables_directory)
 
@@ -227,94 +257,159 @@ class Compiler:
 
     @staticmethod
     def check_pending_sum_sub():
+        """ Compiler handler to check pending sum or substractions.
+        """
         Compiler._interpreter.check_pending_sum_sub()
 
     @staticmethod
     def check_pending_div_prod():
+        """ Compiler handler to check pending division or products..
+        """
         Compiler._interpreter.check_pending_div_prod()
 
     @staticmethod
     def check_pending_rel_op():
+        """ Compiler handler to check pending relational operations.
+        """
         Compiler._interpreter.check_pending_rel_op()
 
     @staticmethod
     def check_pending_and_or():
+        """ Compiler handler to check pending boolean operations.
+        """
         Compiler._interpreter.check_pending_and_or()
 
     @staticmethod
     def maybe_gen_not_quad():
+        """ Compiler handler to check pending not operations..
+        """
         Compiler._interpreter.maybe_gen_not_quad()
 
     @staticmethod
-    def start_condition_quad(isUnless=False):
+    def start_condition_quad(isUnless: bool = False):
+        """ Compiler handler to start a condition.
+
+            Arguments:
+                - is_unless [bool]: Wether is an unless statement
+        """
         Compiler._interpreter.start_condition_quad(isUnless)
 
     @staticmethod
     def end_condition_quad():
+        """ Compiler handler to end a condition.
+        """
         Compiler._interpreter.end_condition_quad()
 
     @staticmethod
     def gen_goto_quad():
+        """ Compiler handler to generate a GOTO quad.
+        """
         Compiler._interpreter.gen_goto_quad()
 
     @staticmethod
     def push_instruction_address():
+        """ Compiler handler to push a instrution address.
+        """
         Compiler._interpreter.push_instruction_address()
 
     @staticmethod
     def push_next_instruction_address():
+        """ Compiler handler to push the next instrution address.
+        """
         Compiler._interpreter.push_next_instruction_address()
 
     @staticmethod
     def end_while_quad():
+        """ Compiler handler to end a loop.
+        """
         Compiler._interpreter.end_while_quad()
 
     @staticmethod
     def read_quad():
+        """ Compiler handler generate a read quad.
+        """
         Compiler._interpreter.read_quad()
 
     @staticmethod
     def write_quad():
+        """ Compiler handler generate a write quad.
+        """
         Compiler._interpreter.write_quad()
 
     @staticmethod
     def return_quad():
+        """ Compiler handler generate a return quad.
+        """
         Compiler._interpreter.return_quad(Compiler._current_method)
 
     @staticmethod
     def start_for_quad():
+        """ Compiler handler start a loop.
+        """
         Compiler._interpreter.start_for_quad()
 
     @staticmethod
     def end_for_quad():
+        """ Compiler handler end a loop.
+        """
         Compiler._interpreter.end_for_quad()
 
     @staticmethod
     def debug_quads():
+        """ Compiler handler to debug quads.
+        """
         Compiler._interpreter.debug_quads()
 
     @staticmethod
     def resolve_dimension_access():
+        """ Compiler handler resolve the access to a dimensional variable.
+        """
         Compiler._interpreter.resolve_dimension_access()
 
     @staticmethod
     def complete_dimension_access():
+        """ Compiler handler complete the access to a dimensional variable.
+        """
         Compiler._interpreter.complete_dimension_access()
 
     @staticmethod
     def allocate_mem_quad(instance, method):
+        """ Compiler handler to generate a ERA quad.
+        """
         Compiler._interpreter.allocate_mem_quad(instance, method)
 
     @staticmethod
-    def add_method_parameter(method, instance = None):
+    def add_method_parameter(method: str, instance: str = None):
+        """ Compiler handler to add a parameter to a method call.
+
+            Arguments:
+                - method [str]: The method being called.
+                - instance [str]: The name of the instance calling the method.
+        """
         Compiler._interpreter.add_method_parameter(Compiler.get_method_scope(method, instance))
 
     @staticmethod
-    def complete_method_call(method, instance = None):
+    def complete_method_call(method, instance: str = None):
+        """ Compiler handler to complete a method call.
+
+            Arguments:
+                - method [str]: The method being called.
+                - instance [str]: The name of the instance calling the method.
+        """
         Compiler._interpreter.complete_method_call(Compiler.get_method_scope(method, instance), instance)
 
     @staticmethod
-    def get_method_scope(method, instance):
+    def get_method_scope(method: str, instance: str):
+        """ Computes the method scope based on the method name and the instance provided.
+
+            Arguments:
+                - method [str]: The method being called.
+                - instance [str]: The name of the instance calling the method.
+
+            Returns:
+                - [MethodScope]: The method scope associated to the provieded
+                    method name and instance.
+        """
         if instance == None:
             # Case when method is the constructor
             class_name = method
